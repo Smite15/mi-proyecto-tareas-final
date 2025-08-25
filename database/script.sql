554b52e3f0215c96497138b0c61b66cc2d1b65c8
Smite15 <saulmite12@gmail.com>
2025-08-24 20:17:20 -0500

    Update script.sql

--- database/script.sql
+++ database/script.sql
@@ -19,7 +19,7 @@ CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  status VARCHAR(50) DEFAULT 'pendiente', -- pendiente en proceso | completedo
+  status VARCHAR(50) DEFAULT 'pendiente', -- pendiente en proceso | completado
   user_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
